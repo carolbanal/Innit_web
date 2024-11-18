887df9460c1c66325272ef13683f95b065ec6ce6
unknown <carolbanal@gmail.com>
2024-11-18 22:08:13 +0800

    Updated URL

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';  // import your HomeScreen component
+import ConfusionMatrix from './screens/ConfusionMatrix';  // import your ConfusionMatrix component
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="p-2 bg-gray-900 flex justify-center items-center space-x-8">
+          <Link to="/" className="p-2 text-white cursor-pointer hover:bg-gray-700 px-4 py-2 rounded">
+            Home
+          </Link>
+          <Link to="/confusion-matrix" className="p-2 text-white cursor-pointer hover:bg-gray-700 px-4 py-2 rounded">
+            Confusion Matrix
+          </Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/confusion-matrix" element={<ConfusionMatrix />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
